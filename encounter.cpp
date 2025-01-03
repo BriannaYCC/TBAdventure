69734725367daa014441b093002e45322e215ce5
Brianna C <briannaycc.2023@gmail.com>
2025-01-02 19:17:34 -0800

    Added encounter class

---
+++ encounter.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Brianna Cabuay on 1/2/2025.
+//
+
+#include "encounter.h"
+encounter::encounter(std::string encounterType) {
+    if (encounterType == "quest") {
+
+    } else if (encounterType == "fight") {
+
+    } else if (encounterType == "NPC") {
+
+    }
+}
---
+++ encounter.h
@@ -0,0 +1,19 @@
+//
+// Created by Brianna Cabuay on 1/2/2025.
+//
+
+#ifndef ENCOUNTER_H
+#define ENCOUNTER_H
+#include <iostream>
+
+
+class encounter {
+public:
+    std::string type; // type of encounter. (quest, monster/fight, NPC interaction)
+    std::string  uh;
+    encounter(std::string encounterType);
+};
+
+
+
+#endif //ENCOUNTER_H
